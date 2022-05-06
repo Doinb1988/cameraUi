b34cec305e864f8bad6addce5f46aaca7b9b6b3d
Andy <andyzhao0808@gmail.com>
2022-05-06 16:55:21 +0700

    添加布局；

--- app/src/main/java/com/example/camera_ui/SecondFragment.kt
+++ app/src/main/java/com/example/camera_ui/SecondFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.camera_ui.databinding.DescriptionBinding
 import com.example.camera_ui.databinding.FragmentSecondBinding
+import com.example.camera_ui.databinding.LensConfigBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,31 +16,35 @@ import com.example.camera_ui.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
+    private var _binding2: DescriptionBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+//    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
-        return binding.root
+        _binding2 = DescriptionBinding.inflate(inflater, container, false)
+        return _binding2!!.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
+//        _binding!!.buttonSecond.setOnClickListener {
+//        }
+//
+        _binding2!!.close.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        _binding2 = null
     }
 }
